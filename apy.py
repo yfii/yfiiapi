f99af7658919554a1d48e126cf99477a56a2a5ed
jin10086 <igaojin@qq.com>
2020-11-06 10:10:37 +0800

    Update apy.py

--- apy.py
+++ apy.py
@@ -91,7 +91,7 @@ def getapy():
     fortube_apy = getfortube()
     apy.update(fortube_apy)
     apy["weth"] = apy["eth"]
-    apy["husd"] = "7.19%"
+    apy["husd"] = "8.90%"
     print(apy)
     with open("apy.json", "w") as f:
         f.write(json.dumps(apy))
